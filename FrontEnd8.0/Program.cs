421ebc16c9ce1065517e0a455f56339536141672
Viet-Creator <nguynviet2006@gmail.com>
2025-12-13 23:20:45 +0700

    add login and register

--- FrontEnd8.0/Program.cs
+++ FrontEnd8.0/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
+builder.Services.AddHttpClient();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,5 +23,4 @@ app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
-
 app.Run();
