b3692f038fae45694dd47136db62b2fb6eff80ff
Viet-Creator <nguynviet2006@gmail.com>
2025-12-14 13:00:18 +0700

    Edit Regis and Login

--- FrontEnd8.0/Components/Model/User.cs
+++ FrontEnd8.0/Components/Model/User.cs
@@ -2,5 +2,11 @@
 {
     public class User
     {
+        public int Id { get; set; }
+        public string Username { get; set; } = "";
+        public string FullName { get; set; } = "";
+        public string PhoneNumber { get; set; } = "";
+        public string Role { get; set; } = "";
     }
+
 }
