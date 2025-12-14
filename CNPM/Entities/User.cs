98426f5a726b9465c73b055f4e533cc6d3591ae4
Lê Anh Tài <taileanh2006@gmail.com>
2025-12-14 11:21:00 +0700

    finish SPRINT1

---
+++ CNPM/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace CNPM.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; } // Lưu ý: Thực tế nên mã hóa, ở đây để plain text cho đơn giản
+        public string PhoneNumber { get; set; }
+        public string FullName { get; set; }
+        public string Role { get; set; } // "Customer" hoặc "HubOwner"
+    }
+}
--- CNPM/Program.cs
+++ CNPM/Program.cs
@@ -1,36 +1,31 @@
+﻿using CNPM.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
-namespace CNPM
+var builder = WebApplication.CreateBuilder(args);
+
+// 1. Cấu hình kết nối SQL Server
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// 2. Cấu hình Controller để tránh lỗi vòng lặp (Circular Reference) khi return Entity có quan hệ
+builder.Services.AddControllers().AddJsonOptions(x =>
+                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+// ... (Các phần Swagger mặc định giữ nguyên)
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// ... (Phần pipeline giữ nguyên)
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
