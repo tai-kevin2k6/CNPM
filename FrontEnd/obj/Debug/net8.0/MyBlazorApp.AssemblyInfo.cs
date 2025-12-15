44b88d2aa040ce72d7152608976dc2d6b8d0f9f5
Yeonyi <parksumin206@gmail.com>
2025-12-15 12:16:55 +0700

    Update

--- FrontEnd/obj/Debug/net8.0/MyBlazorApp.AssemblyInfo.cs
+++ FrontEnd/obj/Debug/net8.0/MyBlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dec2c7ff995666c56b74de4c7f5387ce0a691760")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16d99559fd2f684429826f0dcab005ba1a0c93ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
