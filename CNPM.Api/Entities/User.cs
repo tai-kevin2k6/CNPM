10a03a2585d910e34df3844e41d818a9f88c2ad4
Lê Anh Tài <taileanh2006@gmail.com>
2025-12-31 00:11:13 +0700

    update code

--- CNPM.Api/Entities/User.cs
+++ CNPM.Api/Entities/User.cs
@@ -7,7 +7,7 @@
         public string PhoneNumber { get; set; }
         public string FullName { get; set; }
         public string Role { get; set; } // "Customer" , "HubOwner"
-        public string Status { get; set; }
+        public string? Status { get; set; }
         public string? Reason { get; set; }
     }
     public class UserSummaryDto
